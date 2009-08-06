2fba2669ce825762507bfa6e1b13523b353f8e08
Lawrence Murray <lawrence.murray@csiro.au>
2009-08-06 04:25:17 +0000

    Added NetCDF flags to Makefile script

--- Makefile.pl
+++ Makefile.pl
@@ -8,9 +8,9 @@
 
 $CXX = 'g++';
 $CUDACC = 'nvcc';
-$CXXFLAGS = '-Wall -g -O3 -I"../bi/src"';
-$CUDACCFLAGS = '-O3 -g -arch=sm_13 -Xptxas="-v" -I"../bi/src" -DBOOST_NO_INCLASS_MEMBER_INITIALIZATION -DBOOST_NO_LIMITS_COMPILE_TIME_CONSTANTS';
-$LINKFLAGS = '-L"../bi/build" -lboost_program_options-gcc41-mt -lblas -llapack -lgfortran -lgslcblas -lgsl -lbi';
+$CXXFLAGS = '-Wall -g -O3 -I"../bi/src" `nc-config --cflags`';
+$CUDACCFLAGS = '-O3 -g -arch=sm_13 -Xptxas="-v" -I"../bi/src" -DBOOST_NO_INCLASS_MEMBER_INITIALIZATION -DBOOST_NO_LIMITS_COMPILE_TIME_CONSTANTS `nc-config --cflags`';
+$LINKFLAGS = '-L"../bi/build" `nc-config --libs` -lboost_program_options-gcc41-mt -lblas -llapack -lgfortran -lgslcblas -lgsl -lbi';
 # ^ may need f2c, g2c or nothing in place of gfortran
 $DEPFLAGS = '-I"../bi/src"'; # flags for dependencies check
 
