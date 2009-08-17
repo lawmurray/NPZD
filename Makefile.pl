8b44f8fa16e3649cf87c974725653ef254fa4d58
Lawrence Murray <lawrence.murray@csiro.au>
2009-08-17 03:18:09 +0000

    Corrected head to be configured for cluster by default

--- Makefile.pl
+++ Makefile.pl
@@ -11,7 +11,7 @@ $CUDACC = 'nvcc';
 $LINKER = 'nvcc';
 $CXXFLAGS = '-Wall -g -O3 -I"../bi/src" `nc-config --cflags`';
 $CUDACCFLAGS = '-O3 -g -arch=sm_13 -Xptxas="-v" -I"../bi/src" -I"$GSL_ROOT/include" `nc-config --cflags` -DBOOST_NO_INCLASS_MEMBER_INITIALIZATION -DBOOST_NO_LIMITS_COMPILE_TIME_CONSTANTS';
-$LINKFLAGS = '-L"../bi/build" -lbi -latlas -lf77blas -llapack -lgfortran -lboost_program_options-gcc41-mt -lgslcblas -lgsl `nc-config --libs` -lnetcdf_c++';
+$LINKFLAGS = '-L"../bi/build" -lbi -latlas -lf77blas -llapack -lgfortran -lboost_program_options-gcc43-mt -lgslcblas -lgsl `nc-config --libs` -lnetcdf_c++';
 # ^ may need f2c, g2c or nothing in place of gfortran
 $DEPFLAGS = '-I"../bi/src"'; # flags for dependencies check
 
