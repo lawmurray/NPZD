1e1c3ed0649d592b13fe7abd38c5ff512687f2e4
Lawrence Murray <lawrence.murray@csiro.au>
2009-09-04 09:03:03 +0000

    Fixed Makefile.pl and simulate.sh for cluster again

--- simulate.sh
+++ simulate.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#PBS –l walltime=05:00,nodes=1:ppn=1,gres=gpu -d $PBS_O_WORKDIR
+#PBS -l walltime=10:00,nodes=1:ppn=1,gres=gpu
 
-LD_LIBRARY_PATH=../bi/build:$LD_LIBRARY_PATH
+ROOT=/home/mur387
+LD_LIBRARY_PATH=$ROOT/bi/build:$LD_LIBRARY_PATH
 
+NS=0
 P=10 # no. trajectories
 K=365 # no. output points (should match no. records in forcings file at this stage)
 T=365.0 # time to simulate (again, should match no. records in forcings file)
@@ -10,7 +12,7 @@ SEED=0 # pseudorandom number seed
 OUTPUT=1 # produce output?
 TIME=0 # produce timings?
 
-INPUT_FILE=data/test_input_LC.nc # forcings file
-OUTPUT_FILE=results/output.nc # output file
+INPUT_FILE=$ROOT/data/test_input.nc # forcings file
+OUTPUT_FILE=$ROOT/npzd/results/output.nc # output file
 
-build/simulate -P $P -K $K -T $T --seed $SEED --input-file $INPUT_FILE --output-file $OUTPUT_FILE --output $OUTPUT --time $TIME
+$ROOT/npzd/build/simulate --ns $NS -P $P -K $K -T $T --seed $SEED --input-file $INPUT_FILE --output-file $OUTPUT_FILE --output $OUTPUT --time $TIME
