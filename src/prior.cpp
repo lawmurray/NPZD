ed1316210ddfd7942a8eb43a12895fc407b654ea
Emlyn Jones <emlyn.jones@csiro.au>
2010-02-16 02:35:14 +0000

    Updated variable name in proposal

--- src/prior.cpp
+++ src/prior.cpp
@@ -13,23 +13,22 @@ ConditionalFactoredPdf<GET_TYPELIST(proposalP)> buildPProposal(NPZDModel& m,
     const double scale) {
   ConditionalFactoredPdf<GET_TYPELIST(proposalP)> p;
 
-  p.set(m.KW.id, MultiplicativeLogNormalPdf<>(pow(scale*0.2,2.0)));
-  p.set(m.KC.id, MultiplicativeLogNormalPdf<>(pow(scale*0.3,2.0)));
-  p.set(m.deltaS.id, AdditiveGaussianPdf<>(pow(scale*1.0,2.0)));
-  p.set(m.deltaI.id, MultiplicativeLogNormalPdf<>(pow(scale*0.1,2.0)));
-  p.set(m.P_DF.id, MultiplicativeLogNormalPdf<>(pow(scale*0.2,2.0)));
-  p.set(m.Z_DF.id, MultiplicativeLogNormalPdf<>(pow(scale*0.1,2.0)));
-  p.set(m.alphaC.id, MultiplicativeLogNormalPdf<>(pow(scale*0.63,2.0)));
-  p.set(m.alphaCN.id, MultiplicativeLogNormalPdf<>(pow(scale*0.2,2.0)));
-  p.set(m.alphaCh.id, MultiplicativeLogNormalPdf<>(pow(scale*0.37,2.0)));
-  p.set(m.alphaA.id, MultiplicativeLogNormalPdf<>(pow(scale*1.0,2.0)));
-  p.set(m.alphaNC.id, MultiplicativeLogNormalPdf<>(pow(scale*0.3,2.0)));
-  p.set(m.alphaI.id, MultiplicativeLogNormalPdf<>(pow(scale*0.7,2.0)));
-  p.set(m.alphaCl.id, MultiplicativeLogNormalPdf<>(pow(scale*1.3,2.0)));
-  p.set(m.alphaE.id, MultiplicativeLogNormalPdf<>(pow(scale*0.25,2.0)));
-  p.set(m.alphaR.id, MultiplicativeLogNormalPdf<>(pow(scale*0.5,2.0)));
-  p.set(m.alphaQ.id, MultiplicativeLogNormalPdf<>(pow(scale*1.0,2.0)));
-  //p.set(m.alphaL.id, MultiplicativeLogNormalPdf<>(pow(scale*0.1,2.0)));
+  p.set(m.Kw.id, MultiplicativeLogNormalPdf<>(pow(scale*0.2,2.0)));
+  p.set(m.KCh.id, MultiplicativeLogNormalPdf<>(pow(scale*0.3,2.0)));
+  p.set(m.Dsi.id, AdditiveGaussianPdf<>(pow(scale*1.0,2.0)));
+  p.set(m.ZgD.id, MultiplicativeLogNormalPdf<>(pow(scale*0.1,2.0)));
+  p.set(m.PDF.id, MultiplicativeLogNormalPdf<>(pow(scale*0.2,2.0)));
+  p.set(m.ZDF.id, MultiplicativeLogNormalPdf<>(pow(scale*0.1,2.0)));
+  p.set(m.muPgC.id, MultiplicativeLogNormalPdf<>(pow(scale*0.63,2.0)));
+  p.set(m.muPgR.id, MultiplicativeLogNormalPdf<>(pow(scale*0.2,2.0)));
+  p.set(m.muPCh.id, MultiplicativeLogNormalPdf<>(pow(scale*0.37,2.0)));
+  p.set(m.muPaN.id, MultiplicativeLogNormalPdf<>(pow(scale*1.0,2.0)));
+  p.set(m.muPgR.id, MultiplicativeLogNormalPdf<>(pow(scale*0.3,2.0)));
+  p.set(m.muZin.id, MultiplicativeLogNormalPdf<>(pow(scale*0.7,2.0)));
+  p.set(m.muZCl.id, MultiplicativeLogNormalPdf<>(pow(scale*1.3,2.0)));
+  p.set(m.muZgE.id, MultiplicativeLogNormalPdf<>(pow(scale*0.25,2.0)));
+  p.set(m.muDre.id, MultiplicativeLogNormalPdf<>(pow(scale*0.5,2.0)));
+  p.set(m.muZmQ.id, MultiplicativeLogNormalPdf<>(pow(scale*1.0,2.0)));
 
   return p;
 }
