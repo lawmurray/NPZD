0ec8d2401ff0b85293cba3fa304bec5528dd5414
Lawrence Murray <lawrence.murray@csiro.au>
2010-06-02 10:24:19 +0000

    Updated build defaults for compilation on cluster, updated mcmc.sh script to be more general

--- mcmc.sh
+++ mcmc.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
-#PBS -l walltime=24:00:00,nodes=1:ppn=8,vmem=8gb
+#PBS -l walltime=00:30:00,nodes=4:ppn=8,vmem=8gb
 #PBS -j oe
 
-#source /home/mur387/init.sh
+##
+## System config
+##
+if [[ "`hostname`" == gpu* ]]
+then
+    source init.sh
+    ROOT=$PBS_O_WORKDIR
+    TMP_DIR=$MEMDIR
+    ID=$PBS_JOBNAME
+else
+    ROOT=.
+    TMP_DIR=/tmp
+    ID=mcmc
+fi
+RESULTS_DIR=$ROOT/results
+DATA_DIR=$ROOT/data
 
-ROOT=.
 LD_LIBRARY_PATH=$ROOT/../bi/build:$LD_LIBRARY_PATH
 
+export OMP_NUM_THREADS=2 # no. OpenMP threads per process
+NPERNODE=2 # no. processes per node
+
+##
+## Run config
+##
+
+# MCMC settings
 T=415.0 # time to simulate
 H=1.0 # initial step size
 SEED=3 # pseudorandom number seed
-SCALE=0.1 # scale of initial proposal relative to prior
-ALPHA=0.2 # proportion of proposals that are non-local
+SCALE=0.02 # scale of initial proposal relative to prior
+ALPHA=0.2 # proportion of proposals to be non-local
 SD=0.0 # adaptive proposal parameter (zero triggers default)
-
-B=0 # burn in steps
-I=1 # sample interval (in steps)
-C=40 # no. samples to draw
-A=50 # no. steps before adaptation
+C=100 # no. samples to draw
+A=40 # no. steps before adaptation
 MIN_TEMP=1.0 # minimum temperature (or temperature for single process)
 MAX_TEMP=1.0 # maximum termperature
 
@@ -27,27 +46,26 @@ MIN_ESS=1.0 # minimum ESS to trigger resampling (not used presently)
 P=1024 # no. trajectories
 L=10 # no. iterations for metropolis resampler
 
-# job settings
-ID=0 # job id
-NPROCS=2 # no. processes
+# input files, in $DATA_DIR
+INIT_FILE=C7_init.nc # initial values file
+FORCE_FILE=C7_force_pad.nc # forcings file
+OBS_FILE=C7_S1_obs_padHP2.nc # observations file
 
-DATA_DIR=$ROOT/data
-RESULTS_DIR=$ROOT/results
-TMP_DIR=/tmp
+# output file, in $RESULTS_DIR
+OUTPUT_FILE=$ID.nc
 
-INIT_FILE=$DATA_DIR/C7_init.nc # initial values file
-FORCE_FILE=$DATA_DIR/C7_force_pad.nc # forcings file
-OBS_FILE=$DATA_DIR/C7_S1_obs_pad.nc # observations file
-OUTPUT_FILE=$RESULTS_DIR/mcmc.nc # output file
-FILTER_FILE=$TMP_DIR/pf.nc # intermediate results file
+# intermediate results file 
+FILTER_FILE=$TMP_DIR/pf.nc
 
+# records to use from input files
 INIT_NS=0
 FORCE_NS=0
 OBS_NS=1
 
-# output this script
-#cat $ROOT/npzd/mcmc.sh
+# copy forcings and observations to local directory, used repeatedly
+mpirun -npernode 1 cp $DATA_DIR/$FORCE_FILE $DATA_DIR/$OBS_FILE $TMP_DIR/.
 
-export OMP_NUM_THREADS=2 # no. OpenMP threads per process
+# output this script as record of settings
+cat $ROOT/npzd/mcmc.sh
 
-mpirun -np $NPROCS $ROOT/build/mcmc -P $P -T $T -h $H --min-temp $MIN_TEMP --max-temp $MAX_TEMP --alpha $ALPHA --sd $SD --scale $SCALE --min-ess $MIN_ESS --resampler $RESAMPLER -L $L -C $C -A $A --seed $SEED --init-file $INIT_FILE --force-file $FORCE_FILE --obs-file $OBS_FILE --filter-file $FILTER_FILE --init-ns $INIT_NS --force-ns $FORCE_NS --obs-ns $OBS_NS --output-file $OUTPUT_FILE
+mpirun -npernode $NPERNODE $ROOT/build/mcmc -P $P -T $T -h $H --min-temp $MIN_TEMP --max-temp $MAX_TEMP --alpha $ALPHA --sd $SD --scale $SCALE --min-ess $MIN_ESS --resampler $RESAMPLER -L $L -C $C -A $A --seed $SEED --init-file $DATA_DIR/$INIT_FILE --force-file $TMP_DIR/$FORCE_FILE --obs-file $TMP_DIR/$OBS_FILE --filter-file $FILTER_FILE --init-ns $INIT_NS --force-ns $FORCE_NS --obs-ns $OBS_NS --output-file $RESULTS_DIR/$OUTPUT_FILE
