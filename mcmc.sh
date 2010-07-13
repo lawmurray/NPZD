0570748f0cd3bf176fee5ee286b8d8dcba54d7d1
Lawrence Murray <lawrence.murray@csiro.au>
2010-07-13 03:18:22 +0000

    Updated MCMC script

--- mcmc.sh
+++ mcmc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l walltime=00:30:00,nodes=4:ppn=8,vmem=8gb
+#PBS -l walltime=72:00:00,nodes=16:ppn=8,vmem=16gb
 #PBS -j oe
 
 ##
@@ -9,11 +9,11 @@ if [[ "`hostname`" == gpu* ]]
 then
     source init.sh
     ROOT=$PBS_O_WORKDIR
-    TMP_DIR=$MEMDIR
+    INT_DIR=$MEMDIR
     ID=$PBS_JOBNAME
 else
     ROOT=.
-    TMP_DIR=/tmp
+    INT_DIR=/tmp
     ID=mcmc
 fi
 RESULTS_DIR=$ROOT/results
@@ -29,16 +29,16 @@ NPERNODE=2 # no. processes per node
 ##
 
 # MCMC settings
-T=415.0 # time to simulate
+T=1875.0 # time to simulate
 H=1.0 # initial step size
 SEED=3 # pseudorandom number seed
-SCALE=0.02 # scale of initial proposal relative to prior
-ALPHA=0.2 # proportion of proposals to be non-local
+SCALE=0.05 # scale of initial proposal relative to prior
+ALPHA=0.0 # proportion of proposals to be non-local
 SD=0.0 # adaptive proposal parameter (zero triggers default)
-C=100 # no. samples to draw
-A=40 # no. steps before adaptation
+C=25000 # no. samples to draw
+A=10000 # no. steps before adaptation
 MIN_TEMP=1.0 # minimum temperature (or temperature for single process)
-MAX_TEMP=1.0 # maximum termperature
+MAX_TEMP=1.0 # maximum temperature
 
 # particle filter settings
 RESAMPLER=stratified # resampler to use, 'stratified' or 'metropolis'
@@ -48,14 +48,14 @@ L=10 # no. iterations for metropolis resampler
 
 # input files, in $DATA_DIR
 INIT_FILE=C7_init.nc # initial values file
-FORCE_FILE=C7_force_pad.nc # forcings file
-OBS_FILE=C7_S1_obs_padHP2.nc # observations file
+FORCE_FILE=OSP_71_76_force_pad.nc # forcings file
+OBS_FILE=OSP_71_76_obs_pad.nc # observations file
 
 # output file, in $RESULTS_DIR
 OUTPUT_FILE=$ID.nc
 
 # intermediate results file 
-FILTER_FILE=$TMP_DIR/pf.nc
+FILTER_FILE=$INT_DIR/pf.nc
 
 # records to use from input files
 INIT_NS=0
@@ -63,9 +63,14 @@ FORCE_NS=0
 OBS_NS=1
 
 # copy forcings and observations to local directory, used repeatedly
-mpirun -npernode 1 cp $DATA_DIR/$FORCE_FILE $DATA_DIR/$OBS_FILE $TMP_DIR/.
+mpirun -npernode 1 cp $DATA_DIR/$FORCE_FILE $DATA_DIR/$OBS_FILE $INT_DIR/.
 
 # output this script as record of settings
-cat $ROOT/npzd/mcmc.sh
+cat $ROOT/mcmc.sh
 
-mpirun -npernode $NPERNODE $ROOT/build/mcmc -P $P -T $T -h $H --min-temp $MIN_TEMP --max-temp $MAX_TEMP --alpha $ALPHA --sd $SD --scale $SCALE --min-ess $MIN_ESS --resampler $RESAMPLER -L $L -C $C -A $A --seed $SEED --init-file $DATA_DIR/$INIT_FILE --force-file $TMP_DIR/$FORCE_FILE --obs-file $TMP_DIR/$OBS_FILE --filter-file $FILTER_FILE --init-ns $INIT_NS --force-ns $FORCE_NS --obs-ns $OBS_NS --output-file $RESULTS_DIR/$OUTPUT_FILE
+# sample
+mpirun -npernode $NPERNODE $ROOT/build/mcmc -P $P -T $T -h $H --min-temp $MIN_TEMP --max-temp $MAX_TEMP --alpha $ALPHA --sd $SD --scale $SCALE --min-ess $MIN_ESS --resampler $RESAMPLER -L $L -C $C -A $A --seed $SEED --init-file $DATA_DIR/$INIT_FILE --force-file $INT_DIR/$FORCE_FILE --obs-file $INT_DIR/$OBS_FILE --filter-file $FILTER_FILE --init-ns $INIT_NS --force-ns $FORCE_NS --obs-ns $OBS_NS --output-file $TMPDIR/$OUTPUT_FILE
+
+# copy results from $TMPDIR to $RESULTS_DIR
+mpirun -npernode 1 sh -c "cp $TMPDIR/$OUTPUT_FILE"'*'" $RESULTS_DIR/."
+chmod 644 $RESULTS_DIR/*
