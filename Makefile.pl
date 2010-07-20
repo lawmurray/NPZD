4baff784c1930781dff3afe0cd128658a42c0311
Lawrence Murray <lawrence.murray@csiro.au>
2010-07-20 06:13:25 +0000

    Updated boost locations on cluster

--- Makefile.pl
+++ Makefile.pl
@@ -27,7 +27,7 @@ $CUDACC = 'nvcc';
 $CPPINCLUDES = '-I../bi/src -I/tools/cuda/3.0/cuda/include/ -I/usr/local/cuda/include -I/tools/thrust/1.2 -I/usr/local/include/thrust -I/tools/magma/0.2/include';
 $CXXFLAGS = "-Wall `nc-config --cflags` `mpic++ -showme:compile` $CPPINCLUDES";
 $CUDACCFLAGS = "-arch=sm_13 -Xptxas=\"-v\" -Xcompiler=\"-Wall -fopenmp `mpic++ -showme:compile`\" `nc-config --cflags` -DBOOST_NO_INCLASS_MEMBER_INITIALIZATION -DBOOST_NO_LIMITS_COMPILE_TIME_CONSTANTS $CPPINCLUDES";
-$LINKFLAGS = '-L"../bi/build" -L"/tools/magma/0.2/lib" -lbi -lmagma -lmagmablas -lgfortran -lgsl -lnetcdf_c++ `nc-config --libs` -lpthread -lboost_program_options-gcc43-mt -lboost_mpi-gcc43-mt `mpic++ -showme:link`';
+$LINKFLAGS = '-L"../bi/build" -L"/tools/magma/0.2/lib" -L"/tools/boost/1.43.0/lib" -lbi -lmagma -lmagmablas -lgfortran -lgsl -lnetcdf_c++ `nc-config --libs` -lpthread -lboost_program_options -lboost_mpi `mpic++ -showme:link`';
 # ^ may need f2c, g2c or nothing in place of gfortran
 $DEPFLAGS = '-I"../bi/src"'; # flags for dependencies check
 
