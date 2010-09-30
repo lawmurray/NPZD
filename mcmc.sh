a8460c4bc766d75c7da7d2aed5519ccc39967f48
Lawrence Murray <lawrence.murray@csiro.au>
2010-09-30 08:56:27 +0000

    Updated mcmc run script

--- mcmc.sh
+++ mcmc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l walltime=00:30:00,nodes=4:ppn=8,vmem=8gb
+#PBS -l walltime=24:00:00,nodes=1:ppn=4,vmem=16gb
 #PBS -j oe
 
 ##
@@ -9,10 +9,12 @@ if [[ "`hostname`" == gpu* ]]
 then
     source init.sh
     ROOT=$PBS_O_WORKDIR
-    TMP_DIR=$MEMDIR
+    MEM_DIR=$MEMDIR
+    TMP_DIR=$TMPDIR
     ID=$PBS_JOBNAME
 else
     ROOT=.
+    MEM_DIR=/tmp
     TMP_DIR=/tmp
     ID=mcmc
 fi
@@ -21,7 +23,7 @@ DATA_DIR=$ROOT/data
 
 LD_LIBRARY_PATH=$ROOT/../bi/build:$LD_LIBRARY_PATH
 
-export OMP_NUM_THREADS=2 # no. OpenMP threads per process
+export OMP_NUM_THREADS=4 # no. OpenMP threads per process
 NPERNODE=1 # no. processes per node
 
 ##
@@ -32,11 +34,11 @@ NPERNODE=1 # no. processes per node
 T=1875.0 # time to simulate
 H=1.0 # initial step size
 SEED=20 # pseudorandom number seed
-SCALE=0.05 # scale of initial proposal relative to prior
+SCALE=0.002 # scale of initial proposal relative to prior
 ALPHA=0.2 # proportion of proposals to be non-local
-SD=0.09 # adaptive proposal parameter (zero triggers default)
+SD=0.0 # adaptive proposal parameter (zero triggers default)
 C=100 # no. samples to draw
-A=10000 # no. steps before adaptation
+A=100 # no. steps before adaptation
 MIN_TEMP=1.0 # minimum temperature (or temperature for single process)
 MAX_TEMP=1.0 # maximum temperature
 FILTER=ukf # filter type
@@ -44,8 +46,8 @@ FILTER=ukf # filter type
 # particle filter settings
 RESAMPLER=stratified # resampler to use, 'stratified' or 'metropolis'
 MIN_ESS=1.0 # minimum ESS to trigger resampling (not used presently)
-P=200 # no. trajectories
-L=0 # lookahead for auxiliary particle filter
+P=256 # no. trajectories
+L=1 # lookahead for auxiliary particle filter
 
 # ODE settings
 H=1.0
@@ -56,7 +58,8 @@ EPS_REL=1.0e-3
 #INIT_FILE=PZtest1_init.nc # initial values file
 #FORCE_FILE=C7_force.nc # forcings file
 #OBS_FILE=PZtest1_obs_gaussian.nc # observations file
-INIT_FILE=C7_init.nc # initial values file
+#PROPOSAL_FILE= # proposal file
+INIT_FILE=OSP_71_76_init.nc # initial values file
 FORCE_FILE=OSP_71_76_force_pad.nc # forcings file
 OBS_FILE=OSP_71_76_obs_pad.nc # observations file
 PROPOSAL_FILE=OSP_71_76_proposal.nc # proposal file
@@ -65,18 +68,22 @@ PROPOSAL_FILE=OSP_71_76_proposal.nc # proposal file
 OUTPUT_FILE=$ID.nc
 
 # intermediate results file 
-FILTER_FILE=$TMP_DIR/pf.nc
+FILTER_FILE=filter.nc
 
 # records to use from input files
 INIT_NS=0
 FORCE_NS=0
 OBS_NS=0
 
-# copy forcings and observations to local directory, used repeatedly
-mpirun -npernode 1 cp $DATA_DIR/$FORCE_FILE $DATA_DIR/$OBS_FILE $TMP_DIR/.
+# copy forcings and observations to memory file system, used repeatedly
+mpirun -npernode 1 cp $DATA_DIR/$FORCE_FILE $DATA_DIR/$OBS_FILE $MEM_DIR/.
 
 # output this script as record of settings
-#cat $ROOT/npzd/mcmc.sh
+cat $ROOT/npzd/mcmc.sh
 
-#mpirun -npernode $NPERNODE
-$ROOT/build/mcmc  --type $FILTER -P $P -T $T --min-temp $MIN_TEMP --max-temp $MAX_TEMP --alpha $ALPHA --sd $SD --scale $SCALE --min-ess $MIN_ESS --resampler $RESAMPLER -L $L -C $C -A $A -h $H --eps-abs $EPS_ABS --eps-rel $EPS_REL --seed $SEED --init-file $DATA_DIR/$INIT_FILE --force-file $TMP_DIR/$FORCE_FILE --obs-file $TMP_DIR/$OBS_FILE --proposal-file $DATA_DIR/$PROPOSAL_FILE --filter-file $FILTER_FILE --init-ns $INIT_NS --force-ns $FORCE_NS --obs-ns $OBS_NS --output-file $RESULTS_DIR/$OUTPUT_FILE
+# run
+mpirun -npernode $NPERNODE $ROOT/build/mcmc  --type $FILTER -P $P -T $T --min-temp $MIN_TEMP --max-temp $MAX_TEMP --alpha $ALPHA --sd $SD --scale $SCALE --min-ess $MIN_ESS --resampler $RESAMPLER -L $L -C $C -A $A -h $H --eps-abs $EPS_ABS --eps-rel $EPS_REL --seed $SEED --init-file $DATA_DIR/$INIT_FILE --force-file $MEM_DIR/$FORCE_FILE --obs-file $MEM_DIR/$OBS_FILE --proposal-file $DATA_DIR/$PROPOSAL_FILE --filter-file $FILTER_FILE --init-ns $INIT_NS --force-ns $FORCE_NS --obs-ns $OBS_NS --output-file $TMP_DIR/$OUTPUT_FILE
+
+# copy results from $TMP_DIR to $RESULTS_DIR
+mpirun -npernode 1 sh -c "cp $TMP_DIR/$OUTPUT_FILE"'*'" $RESULTS_DIR/."
+chmod 644 $RESULTS_DIR/*
