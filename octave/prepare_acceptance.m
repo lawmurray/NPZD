18999f5f34062c64a53188452910fb96987528c9
Lawrence Murray <lawrence.murray@csiro.au>
2011-08-05 09:54:56 +0000

    Added qsub and Octave scripts for computing acceptance rates.

---
+++ octave/prepare_acceptance.m
@@ -0,0 +1,48 @@
+% Copyright (C) 2011
+% Author: Lawrence Murray <lawrence.murray@csiro.au>
+% $Rev$
+% $Date$
+
+% -*- texinfo -*-
+% @deftypefn {Function File} prepare_acceptance ()
+%
+% Prepare acceptance rates for PZ model.
+% @end deftypefn
+%
+function prepare_acceptance ()
+    experiments = {'pf', 'mupf', 'cupf', 'apf', 'amupf', 'acupf', ...
+        'pf-pmatch', 'mupf-pmatch', 'cupf', 'apf-pmatch', 'amupf-pmatch', ...
+        'acupf'};
+    invar = invars();
+    M = 200;
+    iter = 2000;
+    attempt = 50;
+    
+    % construct arguments for parallel execution
+    C = length(experiments);
+    files = cell(C,1);
+    invars = cell(C,1);
+    Ms = cell(C,1);
+    coords = cell(C,1);
+    attempts = cell(C,1);
+    iters = cell(C,1);
+    for i = 1:length(experiments)
+        files{i} = glob(sprintf('results/likelihood_%s-[0-9]*.nc.*', ...
+            experiments{i}));
+        invars{i} = invar;
+        coords{i} = [];
+        Ms{i} = M;
+        iters{i} = iter;
+        attempts{i} = attempt;
+    end
+
+    % construct and krig models
+    models = cellfun(@model_acceptance, files, invars, coords, Ms, ...
+        'UniformOutput', 0);
+    models = cellfun(@krig_model, models, iters, 'UniformOutput', 0);
+    mns = cellfun(@min_model, models, attempts, iters, 'UniformOutput', 0);
+    mxs = cellfun(@max_model, models, attempts, iters, 'UniformOutput', 0);
+    
+    % save
+    save -binary model_acceptance.mat models mns mxs
+end
