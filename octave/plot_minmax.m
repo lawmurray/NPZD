c232ec3a4aa04338fa49efb1f25004bf220cf744
Lawrence Murray <lawrence.murray@csiro.au>
2011-08-10 08:28:03 +0000

    Inflated priors over PDF and ZDF for mixing experiments, updated plot_minmax()

--- octave/plot_minmax.m
+++ octave/plot_minmax.m
@@ -10,22 +10,31 @@
 % @end deftypefn
 %
 function plot_minmax (experiment)
+    INIT_FILE = 'data/C10_TE_init.nc';
+    OBS_FILE = 'data/C10_TE_obs.nc';
+
     if nargin < 1
         experiment = 1;
     end
-    
-    load model_acceptance
 
-    % prepare labels
+    load model_acceptance
+    
+    % truth
     vars = invars();
-    vars = map(@nice_greek, vars);
-    for i = 16:length(vars)
-        vars{i} = strcat('{', vars{i}, '_0}');
+    nc = netcdf(INIT_FILE, 'r');
+    truth = zeros(1, 15);
+    for i = 1:30
+        truth(i) = nc{vars{i}}(:);
+        if i != 3
+            truth(i) = log(truth(i));
+        end
     end
     
+    % min and max    
     mn = mns{experiment}(1,:);
     mx = mxs{experiment}(1,:);
-    
+
+    mu = models{experiment}.mu;
     Sigma = models{experiment}.Sigma;
     U = chol(Sigma);
     sd = sqrt(diag(Sigma))';
@@ -35,12 +44,53 @@ function plot_minmax (experiment)
     
     mn = mn./sd;
     mx = mx./sd;
+    truth = (truth - mu)./sd;
     
+    % some reordering for presentation
+    mn = mn(:,[7:15 1:6 16:30]);
+    mx = mx(:,[7:15 1:6 16:30]);
+    truth = truth(:,[7:15 1:6 16:30]);
+    
+    % prepare labels
+    vars = invars();
+    vars = map(@nice_greek, vars);
+    for i = 1:9
+        vars1{i} = vars{i + 6};
+    end
+    for i = 10:15
+        vars1{i} = vars{i - 9};
+    end
+    for i = 16:length(vars)
+        vars2{i - 15} = strcat('{', vars{i}, '_0}');
+    end
+
     % plot
-    bar(mn - mx, 'facecolor', fade(watercolour(2), 0.5), 'edgecolor', ...
+    subplot(2,1,1);
+    h = bar([ mn(1:15); mx(1:15) ]');
+    set(h(1), 'facecolor', fade(gray()(48,:), 0.5), 'edgecolor', ...
+        gray()(48,:));
+    set(h(2), 'facecolor', fade(watercolour(2), 0.5), 'edgecolor', ...
         watercolour(2));
+    hold on;
+    plot(truth(1:15), '.k', 'markersize', 6);
+    hold off;
     plot_defaults;
-    set(gca, 'xtick', 1:length(vars));
-    set(gca, 'xticklabel', vars);
-    ylabel('z-score difference');
+    set(gca, 'xtick', 1:length(vars1));
+    set(gca, 'xticklabel', vars1);
+    legend({'Minimum'; 'Maximum'});
+    ylabel('Prior z-score');
+
+    subplot(2,1,2);
+    h = bar([ mn(16:end); mx(16:end) ]');
+    set(h(1), 'facecolor', fade(gray()(48,:), 0.5), 'edgecolor', ...
+        gray()(48,:));
+    set(h(2), 'facecolor', fade(watercolour(2), 0.5), 'edgecolor', ...
+        watercolour(2));
+    hold on;
+    plot(truth(16:end), '.k', 'markersize', 6);
+    hold off;
+    plot_defaults;
+    set(gca, 'xtick', 1:length(vars2));
+    set(gca, 'xticklabel', vars2);
+    ylabel('Prior z-score');
 end
