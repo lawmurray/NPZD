5c7424e7ad4cd1c607036298e7a141ba7e7d3a25
Lawrence Murray <lawrence.murray@csiro.au>
2013-03-15 08:34:49 +0000

    Added run.sh script, updated config files

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+bi simulate @simulate_osp.conf @config.conf --transform-param-to-state --seed 0 --enable-cuda
+bi sample @sample_osp.conf @config.conf --seed 1
+bi simulate @predict_osp.conf @config.conf --transform-param-to-state --seed 2 --enable-cuda
