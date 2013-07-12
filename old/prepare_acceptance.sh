a68772d140409695c15225f4a9e41a6859685b4b
Lawrence Murray <lawrence.murray@csiro.au>
2013-07-12 16:33:45 +0800

    Removed old files, Eclipse project files, and renamed octave/ directory to standard oct/ directory.

--- old/prepare_acceptance.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#PBS -l walltime=00:30:00,nodes=1:ppn=8,vmem=32gb
-#PBS -j oe
-
-module load octave/3.3.51-gnu
-
-cd $PBS_O_WORKDIR
-octave --path octave --path ../octave --eval "prepare_acceptance()"
