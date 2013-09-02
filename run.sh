55fa970562cafc56f960f72e8d0130fa5076257b
Lawrence Murray <lawrence.murray@csiro.au>
2013-09-02 18:03:22 +0800

    Updated 'bi' to 'libbi' usage in run.sh.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-bi sample @config.conf @prior_osp.conf
-bi sample @config.conf @posterior_osp.conf
-bi sample @config.conf @prediction_osp.conf
+libbi sample @config.conf @prior_osp.conf
+libbi sample @config.conf @posterior_osp.conf
+libbi sample @config.conf @prediction_osp.conf
